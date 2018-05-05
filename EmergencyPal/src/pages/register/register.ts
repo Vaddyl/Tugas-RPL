ffd4fc1d9c3a1d733401b2242191d351e54d1109
Vaddyl <14anonymous03@gmail.com>
2018-05-05 18:58:24 +0700

    register checker

--- EmergencyPal/src/pages/register/register.ts
+++ EmergencyPal/src/pages/register/register.ts
@@ -9,21 +9,46 @@ import { Http } from '@angular/http';
 })
 export class RegisterPage {
 
-  data:any = {};
+  data: any = {};
+  passCheck = false;
+  regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams, public http: Http) {
     this.data.name = '';
     this.data.username = '';
     this.data.email = '';
     this.data.password = '';
-    // this.data.password2 = '';
+    this.data.repassword = '';
     this.data.response = '';
-
     this.http = http;
   }
 
   ionViewDidLoad(){
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  check(){
+    if(this.data.name === '' || this.data.username === '' || this.data.email === '' || this.data.password === '' || this.data.repassword === ''){
+      let alert = this.alertCtrl.create({
+        title: 'Harap mengisi seluruh data!',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else if(this.data.password !== this.data.repassword) {
+      let alert = this.alertCtrl.create({
+        title: 'Password yang anda masukkan berbeda!',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else if(this.regexp.test(this.data.email) == false) {
+      let alert = this.alertCtrl.create({
+        title: 'Mohon periksa kembali email yang anda masukkan!',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else {
+      this.register();
+    }
   }
 
   register(){
@@ -38,9 +63,9 @@ export class RegisterPage {
         buttons: ['OK']
       });
       alert.present();
+      this.navCtrl.push(LoginPage);
     }, error => {
       console.log("Oooops!");
     });
   }
-  
 }
