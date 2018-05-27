2e79e3d535bca05d5ad43b768f86da1af8ec2c64
theinnocent1 <maroghi.yoshi@gmail.com>
2018-05-28 02:04:57 +0700

    front-end: fitur tambah komentar (incomplete)

--- EmergencyPal/src/pages/comments/comments.ts
+++ EmergencyPal/src/pages/comments/comments.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
 
 @IonicPage()
 @Component({
@@ -11,14 +12,23 @@ export class CommentsPage {
   //dummy
   comments = [
     { author: 'Andi', content: 'Kacau nih rumah sakit jir.' },
-    { author: 'Budi', content: 'Tukang parkirnya galak.' }
+    { author: 'Budi', content: 'Tukang parkirnya galak.' },
+    { author: 'Uvuvvuweuweweuweuweu Ogwerlmwefl Ossas', content: 'AweueuwueuweuwueuwwueeuwAweueuwueuweuwueuwwueeuwAweueuwueuweuwueuwwueeuwAweueuwueuweuwueuwwueeuwAweueuwueuweuwueuwwueeuw' }
   ];
+  name: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dataStorage: DataProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommentsPage');
   }
+
+  ionViewWillEnter() {
+    this.dataStorage.getDataUser().then((data) => {
+      // console.log(data);
+      this.name = data.name;
+    })
+   }
 
 }
