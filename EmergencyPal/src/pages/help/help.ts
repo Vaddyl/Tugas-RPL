dc90663e4f30f1bcc175ce462d56f2ac863fa57f
res23adi <restutriadi@gmail.com>
2018-06-16 18:33:30 +0700

    fixed tab help

--- EmergencyPal/src/pages/help/help.ts
+++ EmergencyPal/src/pages/help/help.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-help',
   templateUrl: 'help.html',
 })
 export class HelpPage {
+  information: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+    let localData = this.http.get('assets/information.json').map(res => res.json().items);
+    localData.subscribe(data => {
+      this.information = data;
+    });
+  }
+
+  toggleSection(i) {
+    this.information[i].open = !this.information[i].open; 
+  }
+ 
+  toggleItem(i, j) {
+    this.information[i].children[j].open = !this.information[i].children[j].open;
   }
 
   ionViewDidLoad() {
